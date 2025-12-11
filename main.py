cdaff6a2e21c0635e48835a1817bf20bf7f1dd77
rafis <rafael.hernandez.ochoa95@gmail.com>
2025-12-10 22:55:35 -0600

    chore: remove Daytona integration and uninstall package; remove DAYTONA_API_KEY

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Daytona integration removed.
+
+This file previously used the Daytona SDK to run code in a secure sandbox.
+The SDK and related packages have been uninstalled and references removed.
+
+If you need to restore Daytona integration, reinstall the package and
+restore the original code or update this script to use a different provider.
+"""
+
+def main():
+    print("Daytona integration has been removed from this project.")
+
+
+if __name__ == '__main__':
+    main()
