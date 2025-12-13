e59b02cbe90dad4c16999327f662d3036e6fd492
rafis <rafael.hernandez.ochoa95@gmail.com>
2025-12-12 18:55:16 -0600

    chore(footer-v2): remove console.log debugging - checkpoint 7

--- frontend/static/js/footer-v2-bridge.js
+++ frontend/static/js/footer-v2-bridge.js
@@ -1,4 +1,4 @@
-// Footer V2 Bridge - Integracion segura con Flowing MVP
+﻿// Footer V2 Bridge - Integracion segura con Flowing MVP
 /**
  * Bridge entre Footer V2 y Flowing MVP
  * Permite usar funcionalidades sin modificar Flowing MVP
@@ -6,13 +6,13 @@
 
 class FooterV2Bridge {
   constructor() {
-    console.log('FooterV2Bridge initialized');
+    // console.log('FooterV2Bridge initialized');
     this.init();
   }
   
   init() {
     // Inicializar Footer V2
-    console.log('Footer V2 ready');
+    // console.log('Footer V2 ready');
   }
 }
 
